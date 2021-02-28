27bff192e5c53bb71af3e0accd7e0c4a28440b2c
Alexander Antonov <71496401+SanyaAntonov@users.noreply.github.com>
2021-02-28 07:21:32 +0300

    Add files via upload

---
+++ main/java/ru/antonov/jspcrud/repository/MealRepository.java
@@ -0,0 +1,18 @@
+package ru.antonov.jspcrud.repository;
+
+import ru.antonov.jspcrud.model.Meal;
+
+import java.util.Collection;
+
+public interface MealRepository {
+    // null if not found, when updated
+    Meal save(Meal meal);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Meal get(int id);
+
+    Collection<Meal> getAll();
+}
---
+++ main/java/ru/antonov/jspcrud/web/MealServlet.java
@@ -0,0 +1,71 @@
+package ru.antonov.jspcrud.web;
+
+import ru.antonov.jspcrud.model.Meal;
+import ru.antonov.jspcrud.repository.InMemoryMealRepository;
+import ru.antonov.jspcrud.repository.MealRepository;
+import ru.antonov.jspcrud.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class MealServlet extends HttpServlet {
+
+    private MealRepository repository;
+
+    @Override
+    public void init() {
+        repository = new InMemoryMealRepository();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String id = request.getParameter("id");
+
+        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        repository.save(meal);
+        response.sendRedirect("meals");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        switch (action == null ? "all" : action) {
+            case "delete":
+                int id = getId(request);
+                repository.delete(id);
+                response.sendRedirect("meals");
+                break;
+            case "create":
+            case "update":
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        repository.get(getId(request));
+                request.setAttribute("meal", meal);
+                request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
+                break;
+            case "all":
+            default:
+                request.setAttribute("meals",
+                        MealsUtil.getTos(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+                request.getRequestDispatcher("/meals.jsp").forward(request, response);
+                break;
+        }
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+}
